d92639e0f16bc58f231a2ac14b72dc821bf94717
yinyp <14411410+yyp175@user.noreply.gitee.com>
2024-11-13 10:41:08 +0800

    添加前台页面

--- controller/gen/gen_v1_generate.go
+++ controller/gen/gen_v1_generate.go
@@ -2,10 +2,11 @@ package gen
 
 import (
 	"context"
+	"github.com/gogf/gf/v2/frame/g"
 	"github.com/yinyapeng/hb_temp/api/gen/v1"
 )
 
 func (c *ControllerV1) Generate(ctx context.Context, req *v1.GenerateReq) (res *v1.GenerateRes, err error) {
-
+	g.Dump()
 	return
 }
--- controller/gen/gen_v1_static.go
+++ controller/gen/gen_v1_static.go
@@ -2,7 +2,6 @@ package gen
 
 import (
 	"context"
-	"fmt"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gres"
 	"github.com/yinyapeng/hb_temp/api/gen/v1"
@@ -17,10 +16,10 @@ func (c *ControllerV1) Static(ctx context.Context, req *v1.StaticReq) (res *v1.S
 	} else {
 		router := r.GetRouter(".(html|js|css|png|gif)").String()
 
-		fmt.Println("public/" + router)
-		temp := gres.GetContent("public/" + router)
 		suffix := router[strings.LastIndex(router, ".")+1:]
 		r.Response.Header().Set("content-type", "text/"+suffix+"; charset=utf-8")
+
+		temp := gres.GetContent("public/" + router)
 		r.Response.Writeln(temp)
 	}
 	return
